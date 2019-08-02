14561c9759b0d5805b25c5a607610a25626cbf3f
Thea Barnes <thea@awesomebarnes.com>
2019-08-02 11:02:56 -0400

    Add static root

--- tsbarnes/settings.py
+++ tsbarnes/settings.py
@@ -134,6 +134,7 @@ STATICFILES_FINDERS = (
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 COMPRESS_ROOT = os.path.join(BASE_DIR, 'static', 'min')
