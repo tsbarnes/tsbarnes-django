f0585e6edd33077c2cbbdd7c68d8ebe85135aa23
Thea Barnes <thea@awesomebarnes.com>
2019-08-08 12:43:41 -0400

    Improvements to projects

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from adminsortable.admin import SortableAdmin
 
 from .models import Project
 
 
-class TinyMCEAdmin(admin.ModelAdmin):
+class TinyMCEAdmin(SortableAdmin):
   class Media:
     js = ('/static/js/tiny_mce/tiny_mce.js', '/static/js/tiny_mce/textareas.js',)
 
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from adminsortable.models import SortableMixin
+from resume.models import Job
 
-class Project(models.Model):
+class Project(SortableMixin):
   slug = models.SlugField(blank=False)
   title = models.CharField(max_length=255, blank=False)
   subtitle = models.CharField(max_length=255, blank=True)
@@ -9,10 +11,15 @@ class Project(models.Model):
   short = models.TextField(blank=False)
   text = models.TextField(blank=False)
   technologies = models.CharField(max_length=512, blank=True)
-  date = models.DateField(default=timezone.now)
+  date = models.DateField(default=timezone.now, blank=True)
   created_at=models.DateTimeField(auto_now_add=True)
   updated_at=models.DateTimeField(auto_now=True)
   published = models.BooleanField(default=False)
+  job = models.ForeignKey(Job, on_delete=models.SET_NULL, blank=True, null=True)
+  order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+
+  class Meta:
+      ordering = ['order']
 
   def __unicode__(self):
     return self.title
