a6d961c5eeb3376f6e0927a326f298a03db27f5c
Thea Barnes <thea@awesomebarnes.com>
2019-12-13 21:05:00 -0500

    Added databases to local settings

--- tsbarnes/settings.py
+++ tsbarnes/settings.py
@@ -99,12 +99,15 @@ WSGI_APPLICATION = 'tsbarnes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+try:
+  from .local_settings import DATABASES
+except:
+  DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
   }
-}
 
 
 # Password validation
